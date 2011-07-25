bd7e94a3b39004703911e057c1206247cc87c561
Dominik Russenberger <dominik@russenberger.name>
2011-07-25 06:00:12 -0700

    Edited makejail/jailfiles.sh via GitHub

--- makejail/jailfiles.sh
+++ makejail/jailfiles.sh
@@ -2,14 +2,14 @@
 #
 # (c) terreActive AG
 #
-# $Id: jailfiles.sh 21268 2011-05-12 15:33:27Z dominik $
+# $Id: jailfiles.sh 21301 2011-05-18 14:51:20Z dominik $
 # $HeadURL: http://zak/svn/taclog/branches/TACLOG-2-10-10-feature-OEMspacemgr/tac/jail/jailfiles.sh $
 # Purpose: adds all needed static files to a chroot jail
 #
 # 3456789A123456789B123456789C123456789D123456789E123456789F123456789G12345678
 #
 
-. ./common.sh || echo "FATAL: common.sh not found" >&2
+. ./common.sh || { echo "FATAL: common.sh not found" >&2; exit 2; }
 
 DEST=${1:-''}
 
