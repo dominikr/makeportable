499e635915cc6418f132ad82d4192dabf154bd7e
Dominik Russenberger <dominik@russenberger.name>
2011-07-25 05:59:14 -0700

    newest version

--- makejail/common.sh
+++ makejail/common.sh
@@ -1,12 +1,19 @@
 #
 # (c) terreActive AG
 #
-# $Id: common.sh 21268 2011-05-12 15:33:27Z dominik $
+# $Id: common.sh 21970 2011-07-14 15:14:12Z dominik $
 # $HeadURL: http://zak/svn/taclog/branches/TACLOG-2-10-10-feature-OEMspacemgr/tac/jail/common.sh $
 # Purpose: common functions for the jailbuild tools
 #
 # 3456789A123456789B123456789C123456789D123456789E123456789F123456789G12345678
 #
+
+# early check for bash
+if test -z "$BASH_VERSION"
+then
+	echo "FATAL: This script needs bash to run" >&2
+	exit 2
+fi
 
 function die {
 	echo "ERROR: $1 (${0}, `date`)" >&2
@@ -52,19 +59,19 @@ function install_prog {
 }
 
 
-if [ -z $BASH_VERSION ]; then
-	die "This script needs bash to run"
-elif [ ${BASH_VERSINFO[0]} -lt 3 ]; then
+if [ ${BASH_VERSINFO[0]} -lt 3 ]; then
 	die "This script needs Bash v3 or higher"
 fi
 
 set -o errexit
 set -o pipefail
 set -o nounset
-trap 'die "killed by signal"' INT TERM HUP
-trap 'die "failed command"' ERR
-trap 'die "unexpected exit"' EXIT
 
+#NOTE: we have to include $LINENO here and not in the die command
+#it would else show the line in the die function...
+trap 'die "killed by signal, line $LINENO"' INT TERM HUP
+trap 'die "failed command, line $LINENO"' ERR
+trap 'die "unexpected exit, line $LINENO"' EXIT
 
 #
 # EOF
