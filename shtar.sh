a5c9ec7e545bc4d93af6b2eb631e9b1a3dc15cb3
Dominik Russenberger <dominik@russenberger.name>
2011-07-26 22:07:46 +0200

    new octal handling

--- shtar.sh
+++ shtar.sh
@@ -7,24 +7,20 @@ output(){
 	num=$4
 	reminder=$5
 	num2=$(( $num + 1 ))
-	b=$(( $blocks - 1 ))
-	
+	foo=$(( $num + 1 + $blocks ))	
+
 	if [ -f $name ]; then
 		echo "ERROR: skipping $name, already exists"
 		return
 	fi
 
-	if [ "x$reminder" = "x0" ]; then
-		dd if="${FILE}" count=$blocks skip=$num2 of=$name 2> /dev/null
+	dd if=${FILE} count=$blocks skip=$num2 of=$name 2>/dev/null
+	echo "blocks $blocks skip $num2"
 
-	else
-		if [ $blocks -gt 1 ]; then
-			dd if="${FILE}" count=$b skip=$num2 of=$name 2> /dev/null
-		fi
-		
-		skip2=$(( $num2 + $b ))
-		dd if="${FILE}" count=$blocks skip=$skip2 2> /dev/null | dd bs=1 count=$reminder 2> /dev/null >> $name
+	if [ "x$reminder" != "x0" ]; then
+		dd if="${FILE}" count=1 skip=$foo 2>/dev/null | dd bs=1 count=$reminder 2>/dev/null >> $name
 	fi
+
 	chmod $mode $name
 }
 
@@ -32,19 +28,22 @@ set -e
 FILE=$1
 export FILE
 num=0
-set -o pipefail
+#set -o pipefail
+
 
 while true; do
 
 	tmpf="/tmp/block.$$"
 	dd if="${FILE}" count=1 skip=$num of=${tmpf} 2>/dev/null
 	
-	name=`dd if=${tmpf} bs=99 count=1 2>/dev/null`
-	options=`dd if=${tmpf} bs=1 skip=100 count=157 2>/dev/null`
-	#name=`dd if=${tmpf} bs=99 count=1 2>/dev/null | tr -d '\0'`
-	#options=`dd if=${tmpf} bs=1 skip=100 count=157 2>/dev/null | tr -d '\0'`
-	mode=${options:3:4}
-	size=${options:20:12}
+	#name=`dd if=${tmpf} bs=99 count=1 2>/dev/null`
+	#options=`dd if=${tmpf} bs=1 skip=100 count=157 2>/dev/null`
+	name=`dd if=${tmpf} bs=99 count=1 2>/dev/null | tr -d '\0'`
+	options=`dd if=${tmpf} bs=1 skip=103 count=157 2>/dev/null | tr -d '\0'`
+	mode=${options:0:4}
+	fullblock=${options:17:9}
+	restblock=${options:26:3}
+	echo "SIZE: $fullblock $restblock"
 
 	if [ -z "$name" ]; then
 		echo "zero-size name, EOF?"
@@ -52,20 +51,33 @@ while true; do
 		exit 0
 	fi
 
-	size=$(( 8#$size ))
-	blocks=$(( $size / 512  ))
-	reminder=$(( $size % 512 ))
-	if [ $reminder -gt 0 ]; then
-		blocks=$(( $blocks + 1 ))
-	fi
-
-	#if `echo "$name" | grep '/$' > /dev/null`; then
-	#	mkdir -p $name
-	#else
-	#	output $name $mode $blocks $num $reminder
+	#size=$(( 8#$size ))
+	#blocks=$(( $size / 512  ))
+	#reminder=$(( $size % 512 ))
+	#if [ $reminder -gt 0 ]; then
+	#	blocks=$(( $blocks + 1 ))
 	#fi
 
-	num=$(( $num + 1 + $blocks ))
+
+	blocks=$(( 8#$fullblock ))
+
+	if [ "x$restblock" != "x000" ]; then
+		reminder=$(( 8#$restblock ))
+		rest=1
+	else
+		reminder=0
+		rest=0
+	fi
+
+
+
+	if `echo "$name" | grep '/$' > /dev/null`; then
+		mkdir -p $name
+	else
+		output $name $mode $blocks $num $reminder
+	fi
+
+	num=$(( $num + 1 + $blocks + $rest ))
 	echo $name
 
 done
