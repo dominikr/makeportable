fb027102d1358d00f1cae237300152111f2a5dce
Dominik Russenberger <dominik@russenberger.name>
2011-07-26 19:52:28 +0200

    cleanup and speedup

--- shtar.sh
+++ shtar.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-block(){
-	dd if="${FILE}" count=1 skip=$1 2> /dev/null
-}
-
-
 output(){
 	name=$1
 	mode=$2
@@ -25,31 +20,38 @@ output(){
 	else
 		if [ $blocks -gt 1 ]; then
 			dd if="${FILE}" count=$b skip=$num2 of=$name 2> /dev/null
-			#echo dd if="${FILE}" count=$b skip=$num2 of=$name
 		fi
 		
 		skip2=$(( $num2 + $b ))
 		dd if="${FILE}" count=$blocks skip=$skip2 2> /dev/null | dd bs=1 count=$reminder 2> /dev/null >> $name
-		#echo "dd if=${FILE} count=$blocks skip=$skip2 2> /dev/null | dd bs=1 count=$reminder 2> /dev/null >> $name"
 	fi
 	chmod $mode $name
-	#echo "OUTPUT $name $block $reminder"
 }
 
 set -e
-PATH="/usr/xpg4/bin:$PATH"
-export PATH
 FILE=$1
 export FILE
 num=0
+set -o pipefail
 
 while true; do
 
-	data=`block $num | tr -s '\0' ' '`
+	tmpf="/tmp/block.$$"
+	dd if="${FILE}" count=1 skip=$num of=${tmpf} 2>/dev/null
+	
+	name=`dd if=${tmpf} bs=99 count=1 2>/dev/null`
+	options=`dd if=${tmpf} bs=1 skip=100 count=157 2>/dev/null`
+	#name=`dd if=${tmpf} bs=99 count=1 2>/dev/null | tr -d '\0'`
+	#options=`dd if=${tmpf} bs=1 skip=100 count=157 2>/dev/null | tr -d '\0'`
+	mode=${options:3:4}
+	size=${options:20:12}
 
-	name=`echo $data | awk '{ print $1 }'`
-	size=`echo $data | awk '{ print $5 }'`
-	mode=`echo $data | awk '{ print $2 }' | sed 's/^...//'`
+	if [ -z "$name" ]; then
+		echo "zero-size name, EOF?"
+		rm $tmpf
+		exit 0
+	fi
+
 	size=$(( 8#$size ))
 	blocks=$(( $size / 512  ))
 	reminder=$(( $size % 512 ))
@@ -57,22 +59,13 @@ while true; do
 		blocks=$(( $blocks + 1 ))
 	fi
 
-	if [ -z $name ]; then
-		echo "zero-size name, EOF?"
-		exit 0
-	fi
-
-
-	if `echo "$name" | grep '/$' > /dev/null`; then
-		mkdir -p $name
-	else
-		output $name $mode $blocks $num $reminder
-	fi
-
+	#if `echo "$name" | grep '/$' > /dev/null`; then
+	#	mkdir -p $name
+	#else
+	#	output $name $mode $blocks $num $reminder
+	#fi
 
 	num=$(( $num + 1 + $blocks ))
-	#echo FILE $name $size $blocks $mode
 	echo $name
 
-	#echo -e "\t$data"
 done
