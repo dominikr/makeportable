c3812b93fed50c8678b8306fe23fbbb2e8f498ce
Dominik Russenberger <dominik@russenberger.name>
2011-07-26 22:25:55 +0200

    now down to max 5 calculations per file

--- shtar.sh
+++ shtar.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-output(){
-	name=$1
-	mode=$2
-	blocks=$3
-	num=$4
-	reminder=$5
-	num2=$(( $num + 1 ))
-	foo=$(( $num + 1 + $blocks ))	
-
-	if [ -f $name ]; then
-		echo "ERROR: skipping $name, already exists"
-		return
-	fi
-
-	dd if=${FILE} count=$blocks skip=$num2 of=$name 2>/dev/null
-	echo "blocks $blocks skip $num2"
-
-	if [ "x$reminder" != "x0" ]; then
-		dd if="${FILE}" count=1 skip=$foo 2>/dev/null | dd bs=1 count=$reminder 2>/dev/null >> $name
-	fi
-
-	chmod $mode $name
-}
-
 set -e
 FILE=$1
 export FILE
@@ -36,10 +12,8 @@ while true; do
 	tmpf="/tmp/block.$$"
 	dd if="${FILE}" count=1 skip=$num of=${tmpf} 2>/dev/null
 	
-	#name=`dd if=${tmpf} bs=99 count=1 2>/dev/null`
-	#options=`dd if=${tmpf} bs=1 skip=100 count=157 2>/dev/null`
-	name=`dd if=${tmpf} bs=99 count=1 2>/dev/null | tr -d '\0'`
-	options=`dd if=${tmpf} bs=1 skip=103 count=157 2>/dev/null | tr -d '\0'`
+	name=`dd if=${tmpf} bs=99 count=1 2>/dev/null`
+ 	options=`dd if=${tmpf} bs=1 skip=103 count=157 2>/dev/null | tr -d '\0'`
 	mode=${options:0:4}
 	fullblock=${options:17:9}
 	restblock=${options:26:3}
@@ -50,14 +24,6 @@ while true; do
 		rm $tmpf
 		exit 0
 	fi
-
-	#size=$(( 8#$size ))
-	#blocks=$(( $size / 512  ))
-	#reminder=$(( $size % 512 ))
-	#if [ $reminder -gt 0 ]; then
-	#	blocks=$(( $blocks + 1 ))
-	#fi
-
 
 	blocks=$(( 8#$fullblock ))
 
@@ -70,14 +36,31 @@ while true; do
 	fi
 
 
+	num2=$(( $num + 1 ))
+	foo=$(( $num2 + $blocks ))	
+
 
 	if `echo "$name" | grep '/$' > /dev/null`; then
 		mkdir -p $name
 	else
-		output $name $mode $blocks $num $reminder
+
+		if [ -f $name ]; then
+			echo "ERROR: skipping $name, already exists"
+			return
+		fi
+
+		dd if=${FILE} count=$blocks skip=$num2 of=$name 2>/dev/null
+		echo "blocks $blocks skip $num2"
+
+		if [ "x$reminder" != "x0" ]; then
+			dd if="${FILE}" count=1 skip=$foo 2>/dev/null | dd bs=1 count=$reminder 2>/dev/null >> $name
+		fi
+
+		chmod $mode $name
+
 	fi
 
-	num=$(( $num + 1 + $blocks + $rest ))
+	num=$(( $foo + $rest ))
 	echo $name
 
 done
